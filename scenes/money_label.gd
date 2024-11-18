de07de16da2f946cd3fe5226075e85f091e8ca65
cjo33 <128477453+cjo33@users.noreply.github.com>
2024-11-18 15:57:01 +0000

    Added money and income feature

---
+++ scenes/money_label.gd
@@ -0,0 +1,16 @@
+extends Label
+
+#var pollutionLabel = 0
+@export var YearsTimer : Timer
+@export var MoneyLabel : Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	update_money_label()
+
+func update_money_label() -> void:
+	self.text = str("Money: ", Global.Money)
+
+func _on_years_timer_timeout() -> void:
+	Global.Money += Global.Income
+	update_money_label()
