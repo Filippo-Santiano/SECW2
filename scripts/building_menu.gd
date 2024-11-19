03bb38f38966a1eed79b3dffa8363bb31113f32c
Jake Hulme <joh46@bath.ac.uk>
2024-11-19 10:23:07 +0000

    Building menu bar made a bit prettier

--- scripts/building_menu.gd
+++ scripts/building_menu.gd
@@ -4,12 +4,6 @@ extends Control
 func _process(delta: float) -> void:
 	pass
  
-# Add buildings to a list
-var list_of_buildings = []
-func _ready() -> void:
-	list_of_buildings.append($ScrollContainer/HBoxContainer/BuildingItem)
-	list_of_buildings.append($ScrollContainer/HBoxContainer/Tree_Item)
- 
 # Assign the objects variables to modify
 @export var build_menu_container: HBoxContainer
 @export var build_button: MenuButton
@@ -17,8 +11,17 @@ func _ready() -> void:
 @export var tree_item: Button 
 @onready var build_menu_scene = load("res://scenes/building_menu.tscn")
  
-# Function to hide buttons when buiod icon is pressed
+# Control node to block interactions with map underneath building menu
+var blocker: Control
+
+# Hide the buttons initially
 var is_pressed = false
+func _ready() -> void:
+	#Hide buttons initially
+	building_item.hide()
+	tree_item.hide()
+
+# Hide the buttons when build icon is pressed
 func _on_build_button_pressed() -> void:
 	if is_pressed == true:
 		building_item.show()
