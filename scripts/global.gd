b4ac407aeadf6ad7b3f9a7d6d16ee44e5f852759
Chris <seadonchristopher@gmail.com>
2024-11-24 16:13:09 +0000

    Fixed initial pollution and income data not being added when a tile is built

--- scripts/global.gd
+++ scripts/global.gd
@@ -14,30 +14,30 @@ var Income = 0
 var ExternalPollution = 1
 
 # Fix this, current doesn't update income, fixed pollution levels etc when tiles are built
-func updatePollution():
-	var total_yearly_pollution = 0
-	for i in placed_tiles:
-		total_yearly_pollution += i.yearly_pollution
-	YearlyPollution = total_yearly_pollution
-	#print("Recalculated Yearly Pollution:", Global.YearlyPollution)
-	
-func updateIncome():
-	var total_yearly_income = 0
-	for i in placed_tiles:
-		total_yearly_income += i.yearly_income
-	Income = total_yearly_income
-	
-func updateElectricity():
-	var total_electricity = 0
-	for i in placed_tiles:
-		total_electricity += i.electricity
-	Electricity = total_electricity
-	
-func updateHappiness():
-	var total_happiness = 0
-	for i in placed_tiles:
-		total_happiness += i.happiness
-	Happiness = total_happiness
+#func updatePollution():
+	#var total_yearly_pollution = 0
+	#for i in placed_tiles:
+		#total_yearly_pollution += i.yearly_pollution
+	#YearlyPollution = total_yearly_pollution
+	##print("Recalculated Yearly Pollution:", Global.YearlyPollution)
+	#
+#func updateIncome():
+	#var total_yearly_income = 0
+	#for i in placed_tiles:
+		#total_yearly_income += i.yearly_income
+	#Income = total_yearly_income
+	#
+#func updateElectricity():
+	#var total_electricity = 0
+	#for i in placed_tiles:
+		#total_electricity += i.electricity
+	#Electricity = total_electricity
+	#
+#func updateHappiness():
+	#var total_happiness = 0
+	#for i in placed_tiles:
+		#total_happiness += i.happiness
+	#Happiness = total_happiness
 	
 func updateExternalPollution():
 	var totalExternalPollutionMultiplier = 1.08
@@ -45,24 +45,23 @@ func updateExternalPollution():
 
 	
 	
-	
-func addNewTile(tile,initial_pollution):
-	Pollution += initial_pollution
-	
-	var new_tile = Tile.new() #creates an instance of the Tile class, with
-	new_tile.id = tile #id of the tile being placed.
-	new_tile.initialise_pollution()
-	new_tile.initialise_income() #then sets the pollution/income variables based on the ID
-	new_tile.initialise_electricity()
-	new_tile.initialise_happiness()
-	placed_tiles.append(new_tile) #and adds that tile to the global placed_tiles array.
-	#this is used for adding pollution/income yearly
+	#
+#func addNewTile(tile,initial_pollution):
+	#Pollution += initial_pollution
+	#var new_tile = Tile.new() #creates an instance of the Tile class, with
+	#new_tile.id = tile #id of the tile being placed.
+	#new_tile.initialise_pollution()
+	#new_tile.initialise_income() #then sets the pollution/income variables based on the ID
+	#new_tile.initialise_electricity()
+	#new_tile.initialise_happiness()
+	#placed_tiles.append(new_tile) #and adds that tile to the global placed_tiles array.
+	##this is used for adding pollution/income yearly
 
-func updateData():
-	updatePollution()
-	updateIncome()
-	updateElectricity()	
-	updateHappiness()
+#func updateData():
+	#updatePollution()
+	#updateIncome()
+	#updateElectricity()	
+	#updateHappiness()
 
 var tile_data = {}
 
@@ -74,3 +73,17 @@ func update_tile_attributes():
 			var multipler = tile["multipliers"].get(attr, 0)
 			tile["attributes"][attr] *= pow(1 + tile["multipliers"][attr], years_elapsed)
 		print("Updated tile at", pos, ":", tile["attributes"])  # Debug print
+
+func updateData(x,y):
+	
+	#Called after a tile is placed in the tile_placer scene.
+	#Grabs the attributes from the placed tile in the dictionary and adds them to the current
+	#totals.
+	#Haven't put in happiness or electricity as unsure exactly how the two variables should be utilised
+	
+	var tile = tile_data.get(Vector2(x,y))
+	var attributes = tile.get("attributes")
+	
+	Pollution += attributes.get("yearly_pollution")
+	YearlyPollution += attributes.get("yearly_pollution")
+	Income += attributes.get("income")
--- scripts/tiles.gd
+++ scripts/tiles.gd
@@ -90,7 +90,7 @@ const Initial_Tile_Attributes = {
 	}
 }
 
-const Tile_Multupliers = {
+const Tile_Multipliers = {
 		1: { #Office
 		"yearly_pollution": 0.1, #+10% pollution
 		"income": -0.05,
@@ -108,6 +108,34 @@ const Tile_Multupliers = {
 		"negativeHappiness": 0
 		}
 }
+
+func setInitialAttributes(tile,x,y):
+	if Global.tile_data == null:
+		Global.tile_data = {}
+				
+	var initial_attributes = Initial_Tile_Attributes.get(tile,{
+		"yearly_pollution": 0,
+		"income": 0,
+		"electricityRequired": 0,
+		"electricityGenerated": 0,
+		"positiveHappiness": 0,
+		"negativeHappiness": 0
+	})
+	
+	var multipliers = Tile_Multipliers.get(tile,{
+		"yearly_pollution": 0,
+		"income": 0,
+		"electricityRequired": 0,
+		"electricityGenerated": 0,
+		"positiveHappiness": 0,
+		"negativeHappiness": 0
+	})
+	
+	Global.tile_data[Vector2(x,y)] = {
+		"attributes": initial_attributes.duplicate(true),
+		"multipliers": multipliers.duplicate(true),
+		"placed_time": Global.currentYear
+	}
 
 ## Initialize dynamic pollution value
 #func initialise_pollution():
@@ -164,32 +192,8 @@ func placeTile(tile,x,y):
 				var fixed_pollution = tileToPlace.get_custom_data("Pollution")
 				#Global.addNewTile(tile, fixed_pollution)
 				
-				if Global.tile_data == null:
-					Global.tile_data = {}
+				setInitialAttributes(tile,x,y)
 				
-				var initial_attributes = Initial_Tile_Attributes.get(tile,{
-					"yearly_pollution": 0,
-					"income": 0,
-					"electricityRequired": 0,
-					"electricityGenerated": 0,
-					"positiveHappiness": 0,
-					"negativeHappiness": 0
-				})
-				
-				var multipliers = Tile_Multupliers.get(tile,{
-					"yearly_pollution": 0,
-					"income": 0,
-					"electricityRequired": 0,
-					"electricityGenerated": 0,
-					"positiveHappiness": 0,
-					"negativeHappiness": 0
-				})
-				
-				Global.tile_data[Vector2(x,y)] = {
-					"attributes": initial_attributes.duplicate(true),
-					"multipliers": multipliers.duplicate(true),
-					"placed_time": Global.currentYear
-				}
 			else:
 				print("Not enough molah")
 				
