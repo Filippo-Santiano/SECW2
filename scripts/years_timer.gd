240e286d9b04954bd0c34849f0dd9e20ff5ae764
cjo33 <128477453+cjo33@users.noreply.github.com>
2024-11-25 15:32:58 +0000

    Added comments, fixed multiplier calculation, game quits when lost

--- scripts/years_timer.gd
+++ scripts/years_timer.gd
@@ -14,6 +14,7 @@ var Years = 0.0
 
 signal YearPassed
 
+# Updates the labels each year
 func updateLabels():
 	YearsLabel.text = str("Year: ", int(Global.currentYear))
 	PollutionLabel.text = str("Pollution: ", round(Global.Pollution))
@@ -38,15 +39,21 @@ func _on_timeout() -> void:
 	
 	updateLabels() #update labels every second
 	
+	# Updates the stats evry year
 func update_stats_every_year():
-	print("IM WORKING")
+	
+	# Calls the function to update each tile in the game
 	Global.update_tile_attributes()
+	
+	# Temporary variables so that the labels don't go to zero at the beginning of each year
 	var tempYearlyPollution = 0
 	var tempIncome = 0
 	var tempElectricityReq = 0
 	var tempElectricityGen = 0
 	var tempHappinessPos = 0
 	var tempHappinessNeg = 0
+	
+	# Iterate through each tile's attributes and sums them to temp values
 	for pos in Global.tile_data.keys():
 		var tile = Global.tile_data[pos]
 		tempYearlyPollution += tile["attributes"]["yearly_pollution"]
@@ -55,24 +62,33 @@ func update_stats_every_year():
 		tempElectricityGen += tile["attributes"]["electricityGenerated"]
 		tempHappinessPos += tile["attributes"]["positiveHappiness"]
 		tempHappinessNeg += tile["attributes"]["negativeHappiness"]
+		
+		# If generated is < required, income is decreased by a factor of generated / required
 	if (tempElectricityGen < tempElectricityReq):
+		
+		# # Avoids division by 0
 		if tempElectricityReq != 0:
 			tempIncome *= (tempElectricityGen / tempElectricityReq)
-		 
+	
+	# Updating the global values
 	Global.YearlyPollution = tempYearlyPollution
 	Global.Income = tempIncome
 	Global.Electricity = tempElectricityGen - tempElectricityReq
+	Global.Money += Global.Income
+	Global.Pollution += Global.YearlyPollution
+	Global.Pollution += Global.ExternalPollution
+	
+	# Calculates the happiness percentage 
 	if (tempHappinessPos > tempHappinessNeg):
 		Global.Happiness = 1 #This is 100%
 	else:
 		if tempHappinessNeg != 0:
 			Global.Happiness = (tempHappinessPos/tempHappinessNeg)
-	Global.PollutionThreshold = 1000
-	Global.PollutionThreshold *= Global.Happiness
-	Global.Money += Global.Income
-	Global.Pollution += Global.YearlyPollution
+			
+	# Sets pollution threshold and then calculates pollution threshold based on happiness
+	Global.PollutionThreshold = 1000 * Global.Happiness
 	Global.updateExternalPollution()
-	Global.Pollution += Global.ExternalPollution
+	
 
 var prevYear = 0
 var exceed_threshold_count = 0
@@ -85,15 +101,25 @@ func yearPassed():
 		update_stats_every_year()
 		print("Pollution Threshold: ", Global.PollutionThreshold)
 		if (Global.Money < 0):
+			# prints Bye Bye, not enough mulah
 			print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 			print("Bye Bye, not enough mulah")
 			print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+			get_tree().quit()
+		# If above the threshold, add one to the count, if th count gets above 3, you lose, count resets if you go below
 		elif (Global.Pollution > Global.PollutionThreshold):
 			exceed_threshold_count +=1
+			# prints number of years left
+			print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+			print("Warning, above the threshold. You have ", (3-exceed_threshold_count), "years left")
+			print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 			if (exceed_threshold_count > 3):
+				# prints you have lost the game
 				print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 				print("Bye Bye, too much pollution")
 				print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+				get_tree().quit()
+				
 		else:
 			exceed_threshold_count = 0
 
