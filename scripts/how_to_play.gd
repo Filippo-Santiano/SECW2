ae038c228dcad4218f56ef1dc960efe29bc3537f
Jake Hulme <joh46@bath.ac.uk>
2024-11-29 13:45:29 +0000

    Fixed buttons to switch scenes

--- scripts/how_to_play.gd
+++ scripts/how_to_play.gd
@@ -9,13 +9,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Change to game_goal scence
-func on_continue_pressed():
+func _on_continue_button_pressed() -> void:
 	get_tree().change_scene_to_packed(game_goal)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-# Create connection for continue button
-func handle_connecting_signals() -> void:
-	continue_button.button_down.connect(on_continue_pressed)
--- scripts/welcome_screen.gd
+++ scripts/welcome_screen.gd
@@ -8,14 +8,9 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-# Switch to 'how_to_play' scene when 'continue_button' pressed
-func on_continue_pressed():
+func _on_continue_button_pressed() -> void:
 	get_tree().change_scene_to_packed(how_to_play)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-# Create connection for continue button
-func handle_connecting_signals() -> void:
-	continue_button.button_down.connect(on_continue_pressed)
