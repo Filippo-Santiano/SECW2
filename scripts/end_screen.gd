6a054b170004599d545fed07e6e793830155fb20
tingchang2000 <chang6749@gmail.com>
2024-12-03 11:31:16 +0000

    added four lines in line chart

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -87,18 +87,33 @@ func _draw():
 	
 	var points_happiness = []
 	var points_electricity = []
-	var pollution = []
+	var points_pollution = []
 	var points_money = []
 		# Calculate graph points for the length of one of the data arrays (all arrays will be same length
 	for i in range(happiness_data.size()):
 		
 		var x = start_x + i * step_x
-		var y = start_y - (happiness_data[i]-min_happiness)/(max_happiness-min_happiness)
+		var y = start_y - (happiness_data[i]-min_happiness)/(max_happiness-min_happiness)*700
 		points_happiness.append(Vector2(x, y))
+		
+		var x_electricity = start_x + i * step_x
+		var y_electricity = start_y - (electricity_data[i]-min_electricity)/(max_electricity-min_electricity)*700
+		points_electricity.append(Vector2(x_electricity, y_electricity))
+		
+		var x_pollution = start_x + i * step_x
+		var y_pollution = start_y - (pollution_data[i]-min_pollution)/(max_pollution-min_pollution)*700
+		points_pollution.append(Vector2(x_pollution, y_pollution))
+		
+		var x_money = start_x + i * step_x
+		var y_money = start_y - (money_data[i]-min_money)/(max_money-min_money)*700
+		points_money.append(Vector2(x_money, y_money))
 
 	# Draw the line
 	for j in range(points_happiness.size() - 1):
 		draw_line(points_happiness[j], points_happiness[j + 1], Color(1, 1, 0), 2)
+		draw_line(points_electricity[j], points_electricity[j + 1], Color(1, 0, 0), 2)
+		draw_line(points_pollution[j], points_pollution[j + 1], Color(0, 0, 1), 2)
+		draw_line(points_money[j], points_money[j + 1], Color(0, 1, 0), 2)
 
 	# Draw the X and Y axes
 	draw_line(Vector2(start_x, start_y), Vector2(start_x + graph_width, start_y), Color(1, 1, 1), 2)  # X 
